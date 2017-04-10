2e314b780025e5c8d127a96abeeb60cfada4be97
Pauric Holleran <pauric.holleran@gmail.com>
2017-04-10 04:36:07 +0000

    production setup

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+const express = require('express');
+const app = express();
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/contact', function(req, res) {
+
+  let mailOpts, smtpTrans;
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: "pauric.holleran2@gmail.com",
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+  //Mail options
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
+    to: 'pauric.holleran2@gmail.com',
+    subject: req.body.subject,
+    text: req.body.message
+  };
+
+  smtpTrans.sendMail(mailOpts, function(error, response) {
+
+    //res.sendFile(path.join(__dirname + '/public/index.html'));
+    
+    //Email not sent
+    if (error) {
+      console.log(error);
+    }
+    
+    res.end();
+      //res.sendFile(path.join(__dirname + '/public/index.html'));
+  });
+
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("App listening on port " + process.env.PORT);
+});
