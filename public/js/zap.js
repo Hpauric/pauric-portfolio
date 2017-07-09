23fbb4c77ce0a2c5fa695e7fb8479fb79460a02d
Pauric Holleran <pauric.holleran@gmail.com>
2017-07-09 11:56:15 +0000

    enabled mobile animations

--- public/js/zap.js
+++ public/js/zap.js
@@ -176,7 +176,7 @@ var wow = new WOW({
     boxClass:     'animate',      // animated element css class (default is wow)
     animateClass: 'animated', // animation css class (default is animated)
     offset:       100,          // distance to the element when triggering the animation (default is 0)
-    mobile:       false        // trigger animations on mobile devices (true is default)
+    mobile:       true        // trigger animations on mobile devices (true is default)
 });
 wow.init(); 
 
