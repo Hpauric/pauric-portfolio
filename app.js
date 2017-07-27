33dcc5afd623cf46e51d893abe651e33bb9879ab
Pauric Holleran <pauric.holleran@gmail.com>
2017-07-27 10:16:38 +0000

    gzip compression added

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const nodemailer = require('nodemailer');
-
+const compression = require('compression')
 const bodyParser = require('body-parser');
 
-
+app.use(compression());
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
