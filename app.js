a8492d395d49c8153ccf809878282f2bd06dd905
Pauric Holleran <pauric.holleran@gmail.com>
2019-07-14 21:25:29 +0100

    updated to Senior .NET Developer

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.post('/contact', function(req, res) {
 
 });
 
+process.env.PORT = 3000;
+process.env.IP = "127.0.0.1";
 
 app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("App listening at " + process.env.IP);
   console.log("App listening on port " + process.env.PORT);
 });
